823e969dbc4db6ceb3f702b41d1775798e834137
mabukhashabeh <m.abukahshabeh@sit-mena.com>
2024-12-08 22:00:19 +0300

    refactor (URLs): Name conventions.

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -1,6 +1,6 @@
 from django.urls import include, path
 
 urlpatterns = [
-    path('broker/', include('api.broker.urls')),
+    path('brokers/', include('api.broker.urls')),
     path('offers/', include('api.offer.urls')),
 ]
