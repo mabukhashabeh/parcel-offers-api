5e4649c8c93f187ba840305f297aa1ba4a8768c9
mabukhashabeh <m.abukahshabeh@sit-mena.com>
2024-12-11 01:09:00 +0300

    refactor(task): Parcel monitoring task performance optimization.

--- api/parcel/tasks.py
+++ api/parcel/tasks.py
@@ -2,7 +2,7 @@ import logging
 
 import requests
 from celery import shared_task
-from django.db.models import Count, F, Q
+from django.db.models import Count, F, Q, Value
 
 from api.parcel.models import Parcel
 
@@ -15,26 +15,20 @@ def monitor_parcel_offers_task():
     Periodic task to monitor parcels and send notifications when all parcels in
     a combination (block_number + subdivision_number) have active offers.
     """
-    parcels = (
+    parcels_with_active_offers = (
         Parcel.objects.values("block_number", "subdivision_number")
         .annotate(
             total_count=Count("id"),
             active_count=Count("id", filter=Q(offers__isnull=False)),
         )
         .filter(total_count=F("active_count"))
-    )  # only combinations where all are has active offers
+        .annotate(
+            message=Value("All parcels in this combination have active offers.")
+        ).values("block_number", "subdivision_number", "message")
+    )
 
-    notifications = [
-        {
-            "block_number": parcel["block_number"],
-            "subdivision_number": parcel["subdivision_number"],
-            "message": "All parcels in this combination have active offers.",
-        }
-        for parcel in parcels
-    ]
-
-    if len(notifications) > 0:
-        send_bulk_notifications(notifications)  # send bulk dummy notifications
+    if notifications:= list(parcels_with_active_offers):
+        send_bulk_notifications(notifications)
 
 
 def send_bulk_notifications(notifications):
